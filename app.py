0338cef3db554fa2fb19a5f9592b65253ab817b9
“derrick” <derrickmbugua50@gmail.com>
2023-03-26 13:56:05 +0300

    feat: mysql database setup

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote
+
+# create the extension
+db = SQLAlchemy()
 
 app = Flask(__name__)
 
+# configure the mysql database, relative to the app instance folder
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mwema:%s@localhost/god_in_a_box'% quote('Mwema-1234')
+#secret key
+app.config['Secret_key'] = "my long secret key veryy"
+# initialize the app with the extension
+db.init_app(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html')
+  
+  
+@app.route('/login')
+def login():
+  return render_template('login.html')
+
+@app.route('/register')
+def register():
+  return render_template('register.html')
